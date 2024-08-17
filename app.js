22a02101a149271d4331bf1949cb66bb93c4f504
jubayer <jubayerahmed46@gmail.com>
2024-08-17 17:15:59 +0600

    added loading effect

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const navs = document.querySelector('nav ul');
 const closeNav = document.querySelector('#closeNav');
 const mobileMenu = document.querySelector('#mobileMenu');
+const Loader = document.querySelector('#loader');
+window.onload = function() {
+    Loader.style.display = 'none';
+    document.querySelector('#hasLoad').setAttribute('href', './loaded.css')
+}
 
 mobileMenu.addEventListener('click', function() {
     navs.setAttribute('id', 'navs')
